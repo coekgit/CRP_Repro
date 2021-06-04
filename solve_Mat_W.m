f9820381d4b0b24101dd76e6b498b3e26f32146c
coekgit <807153011@163.com>
2021-06-04 23:37:53 +0800

    修改函数参数

--- solve_Mat_W.m
+++ solve_Mat_W.m
@@ -1,11 +1,11 @@
-function [W] = solve_Mat_W(Train_SET,lambda)
-[n_rows,n_columns] = size(Train_SET);
+function [W] = solve_Mat_W(X,lambda)
+[n_rows,n_columns] = size(X);
 W = zeros(n_columns,n_columns);
 delete(gcp('nocreate'))
 N = maxNumCompThreads;
 parpool(N);
 parfor idx = 1:n_columns
     idx;
-    [w_i] = solve_wi(idx,Train_SET,lambda);
+    [w_i] = solve_wi(idx,X,lambda);
     W(:,idx) = w_i;
 end
