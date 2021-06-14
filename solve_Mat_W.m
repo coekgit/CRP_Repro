08860de1cef22aa9d90561b0a89b1e11edf15539
coekgit <807153011@163.com>
2021-06-14 10:23:42 +0800

    删除中间输出

--- solve_Mat_W.m
+++ solve_Mat_W.m
@@ -3,7 +3,6 @@ function [W] = solve_Mat_W(X,lambda)
 W = zeros(n_columns,n_columns);
 if n_columns <200
     for idx = 1:n_columns
-        idx
         [w_i] = solve_wi(idx,X,lambda);
         W(:,idx) = w_i;
     end
